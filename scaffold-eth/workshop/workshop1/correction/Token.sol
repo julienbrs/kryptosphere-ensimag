8475240617de5b594baad4e098fa08652258a46f
julienbrs <julien.bourseau@gmail.com>
2023-03-19 18:58:37 +0100

    certains commentaires enlevés

--- scaffold-eth/workshop/workshop1/correction/Token.sol
+++ scaffold-eth/workshop/workshop1/correction/Token.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.0;
 
 contract Token {
     // Déclaration des variables d'état
-    string public name; // nom du token
-    string public symbol; // symbole du token
-    uint256 public totalSupply; // offre totale de tokens
-    mapping(address => uint256) public balanceOf; // solde de chaque adresse en nombre de tokens
-    address public admin; // adresse de l'administrateur du contrat
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    address public admin;
 
     // Déclaration des événements
     event Transfer(address from, address to, uint256 value); // événement émis lors d'un transfert de tokens
@@ -35,16 +35,16 @@ contract Token {
     function transfer(address _to, uint256 _value) public {
         require(balanceOf[msg.sender] >= _value, "Insufficient balance"); // vérification que l'émetteur a assez de tokens
         require(_to != address(0), "Invalid address"); // vérification que le destinataire n'est pas l'adresse nulle (0x0)
-        balanceOf[msg.sender] -= _value; // soustraction du montant du solde de l'émetteur
-        balanceOf[_to] += _value; // ajout du montant au solde du destinataire
-        emit Transfer(msg.sender, _to, _value); // émission de l'événement Transfer avec les paramètres correspondants
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
     }
 
     // Fonction qui permet à l'administrateur de créer des nouveaux tokens et les ajouter au solde du bénéficiaire
     function mint(address _to, uint256 _value) public onlyAdmin {
         require(_to != address(0), "Invalid address");
-        totalSupply += _value; // augmentation de l'offre totale de tokens
-        balanceOf[_to] += _value; // ajout du montant au solde du bénéficiaire
+        totalSupply += _value;
+        balanceOf[_to] += _value;
         emit Transfer(address(0), _to, _value); // émission de l'événement Transfer avec l'adresse nulle comme émetteur
     }
 
@@ -52,13 +52,13 @@ contract Token {
     function burn(address _from, uint256 _value) public onlyAdmin {
         require(balanceOf[_from] >= _value, "Insufficient balance");
         require(_from != address(0), "Invalid address");
-        totalSupply -= _value; // diminution de l'offre totale de tokens
-        balanceOf[_from] -= _value; // soustraction du montant du solde du bénéficiaire
+        totalSupply -= _value;
+        balanceOf[_from] -= _value;
         emit Transfer(_from, address(0), _value); // émission de l'événement Transfer avec l'adresse nulle comme destinataire
     }
 
     // Fonction qui permet de consulter le solde d'une adresse en nombre de tokens
     function getBalance(address _account) public view returns (uint256) {
-        return balanceOf[_account]; // retourne la valeur du mapping balanceOf pour l'adresse donnée
+        return balanceOf[_account];
     }
 }
